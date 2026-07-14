c341e7f44e01035dd3ef215120eae4eb185e063f
Hariprasath L V <hariprasathdlhdlh@gmail.com>
2026-07-14 13:59:26 +0530

    Create 2507-NumberOfCommonFactors

---
+++ 2507-NumberOfCommonFactors/2507-NumberOfCommonFactors.java
@@ -0,0 +1,12 @@
+// Last updated: 14/07/2026, 13:59:25
+class Solution {
+    public int commonFactors(int a, int b) {
+        int count=0;
+        for(int i=1; i<=a&i<=b; i++){
+            if((a%i==0) && (b%i==0)){
+                count++;
+            }
+        }
+        return count;
+    }
+}
