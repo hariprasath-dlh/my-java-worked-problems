e31b2dc4123c907f359f4532806702aba804d58a
Hariprasath L V <hariprasathdlhdlh@gmail.com>
2026-07-14 17:38:30 +0530

    Delete 2812-FindTheMaximumAchievableNumber directory

--- 2812-FindTheMaximumAchievableNumber/2812-FindTheMaximumAchievableNumber.java
+++
@@ -1,8 +0,0 @@
-// Last updated: 14/07/2026, 13:59:16
-class Solution {
-    public int theMaximumAchievableX(int n, int t) {
-        int sum =n+t;
-        int ans =sum+t;
-        return ans;
-    }
-}
