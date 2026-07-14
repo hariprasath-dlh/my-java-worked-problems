a5c9af822d85b97fee7020739d1280dbe1139652
Hariprasath L V <hariprasathdlhdlh@gmail.com>
2026-07-14 17:38:43 +0530

    Delete 2955-AccountBalanceAfterRoundedPurchase directory

--- 2955-AccountBalanceAfterRoundedPurchase/2955-AccountBalanceAfterRoundedPurchase.java
+++
@@ -1,9 +0,0 @@
-// Last updated: 14/07/2026, 13:59:10
-class Solution {
-    public int accountBalanceAfterPurchase(int p) {
-     int n=((p + 5) / 10) * 10;
-     int ans = 100-n;
-     return ans;
-    }
-    
-}
