e85a56a382988434a3e5f870fa33889ceb43d449
Hariprasath L V <hariprasathdlhdlh@gmail.com>
2026-07-14 13:59:17 +0530

    Create 2812-FindTheMaximumAchievableNumber

---
+++ 2812-FindTheMaximumAchievableNumber/2812-FindTheMaximumAchievableNumber.java
@@ -0,0 +1,8 @@
+// Last updated: 14/07/2026, 13:59:16
+class Solution {
+    public int theMaximumAchievableX(int n, int t) {
+        int sum =n+t;
+        int ans =sum+t;
+        return ans;
+    }
+}
