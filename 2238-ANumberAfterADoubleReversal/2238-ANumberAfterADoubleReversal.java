6e534f80919a363dec6e7daf60c23202b1eaa252
Hariprasath L V <hariprasathdlhdlh@gmail.com>
2026-07-14 17:35:09 +0530

    Delete 2238-ANumberAfterADoubleReversal directory

--- 2238-ANumberAfterADoubleReversal/2238-ANumberAfterADoubleReversal.java
+++
@@ -1,9 +0,0 @@
-// Last updated: 14/07/2026, 13:59:30
-class Solution {
-    public boolean isSameAfterReversals(int num) {
-        if(num==0){
-            return true;
-        }
-        return !(num%10==0);
-    }
-}
